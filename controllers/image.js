c8873a23c9c5dd5a22b1d18a48cee083c9d3f29d
Martin Trembac <martintrembac052@gmail.com>
2023-07-08 19:49:32 +0100

    My first commit

---
+++ controllers/image.js
@@ -0,0 +1,54 @@
+// const Clarifai = require('clarifai');
+const returnClarifaiRequestOptions = (imageUrl) => {
+    const USER_ID = 'martin0521992';//(the code by your name)
+    const PAT = '0f7e8cf552b142229f8f2332f504919b';//(your Clarifai api key)
+    const APP_ID = 'Face_detect';//(what you named your app in Clarifai)
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
+    const IMAGE_URL = imageUrl;
+    // const IMAGE_URL = this.state.input;
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [{ "data": { "image": { "url": IMAGE_URL } } }]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            // eslint-disable-next-line
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    }
+    console.log(requestOptions);
+    return requestOptions;
+};
+
+const handleApiCall = (req, res) => {
+    console.log("imageurl: ", req.body.input)
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(req.body.input))
+        .then(response => response.json())
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json('unable to work with api'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+
+};
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+// app.use(bodyParser.json());
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'martin',
+        password: '',
+        database: 'smart-brain'
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Success');
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+// bcrypt.hash("bacon", null, null, function(err, hash) {
+//     // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
+app.listen(3000, () => {
+    console.log('app is running on log 3000.');
+})
+
+/*
+/--> res = this is working
+/signin --> POST request = success / fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
