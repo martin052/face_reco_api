9c074b464aa1df3e498abec445b2c70408ca25de
Martin Trembac <martintrembac052@gmail.com>
2023-07-09 18:57:52 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@
 const returnClarifaiRequestOptions = (imageUrl) => {
     const USER_ID = 'martin0521992';//(the code by your name)
     const PAT = '0f7e8cf552b142229f8f2332f504919b';//(your Clarifai api key)
-    const APP_ID = 'Face_detect';//(what you named your app in Clarifai)
+    const APP_ID = 'Face_detection';//(what you named your app in Clarifai)
     const MODEL_ID = 'face-detection';
     const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
     const IMAGE_URL = imageUrl;
