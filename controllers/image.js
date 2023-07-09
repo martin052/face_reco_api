7ce5ab96f52f919a48c9c15034120ac494de6e1a
Martin Trembac <martintrembac052@gmail.com>
2023-07-09 19:37:08 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -1,5 +1,5 @@
 // const Clarifai = require('clarifai');
-const axios = require('axios');
+// const axios = require('axios');
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
 const returnClarifaiRequestOptions = (imageUrl) => {
