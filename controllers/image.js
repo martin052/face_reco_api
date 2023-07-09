40fb349a721346f42189bb110eee0c7a9e141223
Martin Trembac <martintrembac052@gmail.com>
2023-07-09 19:58:17 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -1,48 +1,68 @@
 // const Clarifai = require('clarifai');
-const MODEL_ID = 'face-detection';
-const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
-const returnClarifaiRequestOptions = (imageUrl) => {
-    const USER_ID = 'martin0521992';//(the code by your name)
-    const PAT = '0f7e8cf552b142229f8f2332f504919b';//(your Clarifai api key)
-    const APP_ID = 'Face_detect';//(what you named your app in Clarifai)
-
-    const IMAGE_URL = imageUrl;
-
-
-    // const IMAGE_URL = this.state.input;
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [{ "data": { "image": { "url": IMAGE_URL } } }]
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            // eslint-disable-next-line
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    }
-    console.log(requestOptions);
-    return requestOptions;
+const IMAGE_URL = req.body.id;
+const raw = {
+    "user_app_id": {
+        "user_id": 'martin0521992',
+        "app_id": 'Face_detect'
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
 };
 
-const handleApiCall = (req, res, axios) => {
-    console.log("imageurl: ", req.body.input)
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs",
-        returnClarifaiRequestOptions(req.body.input))
+// const MODEL_ID = 'face-detection';
+// const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
+// const returnClarifaiRequestOptions = (imageUrl) => {
+//     const USER_ID = 'martin0521992';//(the code by your name)
+//     const PAT = '0f7e8cf552b142229f8f2332f504919b';//(your Clarifai api key)
+//     const APP_ID = 'Face_detect';//(what you named your app in Clarifai)
+
+//     const IMAGE_URL = imageUrl;
+
+
+// const IMAGE_URL = this.state.input;
+//     const raw = JSON.stringify({
+//         "user_app_id": {
+//             "user_id": USER_ID,
+//             "app_id": APP_ID
+//         },
+//         "inputs": [{ "data": { "image": { "url": IMAGE_URL } } }]
+//     });
+
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             // eslint-disable-next-line
+//             'Authorization': 'Key ' + PAT
+//         },
+//         body: raw
+//     }
+//     console.log(requestOptions);
+//     return requestOptions;
+// };
+
+const handleApiCall = (req, res) => {
+    axios.post("https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs",
+        raw,
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT
+            }
+        }
+    )
         .then(response => {
-            console.log(response.data); // Log the response data
-            res.json(response.data); // Send the response data to the client
+            res.json(response.data)
+
         })
-        .catch(err => {
-            console.log(err);
-            res.status(400).json('Unable to work with the API');
-        });
+        .catch(err => res.status(400).json('unable to work with api'))
     // .then(response => response.json())
     // .then(data => res.json(data))
     // .catch(err => res.status(400).json('unable to work with api'))
@@ -50,17 +70,16 @@ const handleApiCall = (req, res, axios) => {
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
-    db('users').where('id', '=', id)
+    db('users')
+        .where('id', '=', id)
         .increment('entries', 1)
         .returning('entries')
         .then(entries => {
-            res.json(entries[0].entries);
+            res.json(entries[0].entries)
         })
-        .catch(err => res.status(400).json('unable to get entries'))
+        .catch((err) => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
-    handleImage: handleImage,
-    handleApiCall: handleApiCall
-
-};
+    handleApiCall, handleImage
+}
