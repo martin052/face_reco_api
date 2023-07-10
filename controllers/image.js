ca193c5ce507d68708d1cd0abda03edec088a0d2
Martin Trembac <martintrembac052@gmail.com>
2023-07-10 21:30:16 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -1,38 +1,4 @@
-// const Clarifai = require('clarifai');
 const axios = require('axios');
-
-
-// const MODEL_ID = 'face-detection';
-// const MODEL_VERSION_ID = '5e026c5fae004ed4a83263ebaabec49e';
-// const returnClarifaiRequestOptions = (imageUrl) => {
-//     const USER_ID = 'martin0521992';//(the code by your name)
-//     const PAT = '0f7e8cf552b142229f8f2332f504919b';//(your Clarifai api key)
-//     const APP_ID = 'Face_detect';//(what you named your app in Clarifai)
-
-//     const IMAGE_URL = imageUrl;
-
-
-// const IMAGE_URL = this.state.input;
-//     const raw = JSON.stringify({
-//         "user_app_id": {
-//             "user_id": USER_ID,
-//             "app_id": APP_ID
-//         },
-//         "inputs": [{ "data": { "image": { "url": IMAGE_URL } } }]
-//     });
-
-//     const requestOptions = {
-//         method: 'POST',
-//         headers: {
-//             'Accept': 'application/json',
-//             // eslint-disable-next-line
-//             'Authorization': 'Key ' + PAT
-//         },
-//         body: raw
-//     }
-//     console.log(requestOptions);
-//     return requestOptions;
-// };
 
 const handleApiCall = (req, res) => {
     const PAT = '4d99b5e10ec64aabb3220b963707a62f';
@@ -72,9 +38,6 @@ const handleApiCall = (req, res) => {
         });
 };
 
-
-
-
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users')
@@ -82,11 +45,12 @@ const handleImage = (req, res, db) => {
         .increment('entries', 1)
         .returning('entries')
         .then(entries => {
-            res.json(entries[0].entries)
+            res.json(entries[0].entries);
         })
-        .catch((err) => res.status(400).json('unable to get entries'))
-}
+        .catch(err => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
-    handleApiCall, handleImage
-}
+    handleApiCall,
+    handleImage
+};
