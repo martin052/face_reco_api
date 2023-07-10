5e618e525e248c68ca7f5fd5139410bb779c2d04
Martin Trembac <martintrembac052@gmail.com>
2023-07-10 21:11:25 +0100

    My first commitsdfsd

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-
-const app = express();
-// app.use(bodyParser.json());
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const register = require('./controllers/register');
@@ -10,20 +6,10 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-// const db = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         host: 'dpg-cikroc15rnuvtgrauuvg-a',
-//         port: 5432,
-//         user: 'postgresdata_rsx6_user',
-//         password: 'INpgwGHOnFydwhv9pPeMUP066ydpok6D',
-//         database: 'postgres://postgresdata_rsx6_user:INpgwGHOnFydwhv9pPeMUP066ydpok6D@dpg-cikroc15rnuvtgrauuvg-a/postgresdata_rsx6'
-//     }
-// });
 
 const db = require('knex')({
     client: 'pg',
@@ -33,20 +19,38 @@ const db = require('knex')({
     }
 });
 
+// Allow requests from specific origin
+const corsOptions = {
+    origin: 'https://myfacerecognitionweb.onrender.com',
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Success');
-})
+});
 
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/signin', (req, res) => {
+    signin.handleSignin(req, res, db, bcrypt);
+});
 
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post('/register', (req, res) => {
+    register.handleRegister(req, res, db, bcrypt);
+});
 
-app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+app.get('/profile/:id', (req, res) => {
+    profile.handleProfile(req, res, db);
+});
 
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.put('/image', (req, res) => {
+    image.handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+    image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
-})
+});
