37a2e461f92bdef994ada60a26be04c438c18847
Martin Trembac <martintrembac052@gmail.com>
2023-07-11 00:05:35 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -48,13 +48,14 @@ const handleApiCall = (req, res, axios) => {
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
-    db('users').where('id', '=', id)
+    db('users')
+        .where('id', '=', id)
         .increment('entries', 1)
         .returning('entries')
         .then(entries => {
-            res.json(entries[0].entries);
+            res.json(entries[0]); // Return entries[0] instead of entries[0].entries
         })
-        .catch(err => res.status(400).json('unable to get entries'))
+        .catch(err => res.status(400).json('unable to get entries'));
 }
 
 module.exports = {
