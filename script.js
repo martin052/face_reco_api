1183388c7490298c829df7b6761cb1f4bc00c71e
Martin Trembac <martintrembac052@gmail.com>
2023-07-10 23:27:47 +0100

    My first commitsdfsd

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const { handleRegister } = require('./controllers/register');
-const { handleSignin } = require('./controllers/signin');
-const { handleProfile } = require('./controllers/profile');
-const { handleApiCall, handleImage } = require('./controllers/image');
-
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -32,21 +31,19 @@ const db = require('knex')({
 app.get('/', (req, res) => { res.send("Success") })
 
 //SIGNIN (post, pw over HTTP body)
-app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) });
-
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
 
 //REGISTER (post, add to database)
-app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
 //PROFILE (get user)
-app.get('/profile/:id', (req, res) => { handleProfile(req, res, db) });
-
+app.get('/profile/:id', (req, res, db) => { profile.handleProfileGet(req, res, db) });
 
 //IMAGE (put, update count on user profile)
-app.put('/image', (req, res) => { handleImage(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
-app.post('/imageurl', (req, res) => { handleApiCall(req, res) });
-
+//IMAGEURL (post, handle Face Recognition API from backend)
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
