ab7ddb1f1b1ab67f599401c8a420d0a6b0052277
Martin Trembac <martintrembac052@gmail.com>
2023-07-11 20:19:41 +0100

    My first commitsdfsd

--- controllers/image.js
+++ controllers/image.js
@@ -52,7 +52,6 @@ const handleApiCall = (req, res) => {
         ]
     };
 
-
     axios.post("https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs",
         raw,
         {
@@ -63,17 +62,55 @@ const handleApiCall = (req, res) => {
         }
     )
         .then(response => {
-            console.log(response.data); // Log the response data
-            res.json(response.data); // Send the response data to the client
+            console.log(response.data);
+            res.json(response.data);
         })
         .catch(err => {
             console.log(err);
             res.status(400).json('Unable to work with the API');
         });
-    // .then(response => response.json())
-    // .then(data => res.json(data))
-    // .catch(err => res.status(400).json('unable to work with api'))
 }
+// const handleApiCall = (req, res) => {
+//     const PAT = '0f7e8cf552b142229f8f2332f504919b';
+//     const IMAGE_URL = req.body.id;
+//     const raw = {
+//         "user_app_id": {
+//             "user_id": 'martin0521992',
+//             "app_id": 'Face_detect'
+//         },
+//         "inputs": [
+//             {
+//                 "data": {
+//                     "image": {
+//                         "url": IMAGE_URL
+//                     }
+//                 }
+//             }
+//         ]
+//     };
+
+
+//     axios.post("https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs",
+//         raw,
+//         {
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Authorization': 'Key ' + PAT
+//             }
+//         }
+//     )
+//         .then(response => {
+//             console.log(response.data); // Log the response data
+//             res.json(response.data); // Send the response data to the client
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(400).json('Unable to work with the API');
+//         });
+//     // .then(response => response.json())
+//     // .then(data => res.json(data))
+//     // .catch(err => res.status(400).json('unable to work with api'))
+// }
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
